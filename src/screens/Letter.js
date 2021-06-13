e8279f0313cabd362a01f0a0307b7e4b188ff8ed
junzzi <jjh01027717502@gmail.com>
2021-06-14 03:44:41 +0900

    feat

--- src/screens/Letter.js
+++ src/screens/Letter.js
@@ -4,7 +4,19 @@ import { Link, withRouter, Redirect } from "react-router-dom";
 import axios from "axios";
 
 import { SERVER_URL } from "../utils/URL";
-
+import styled from "styled-components";
+const Wrapper = styled.div`
+  min-height: 100vh;
+`;
+const Inner = styled.div`
+  max-width: 700px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 90vh;
+`;
 class Letter extends React.Component {
   constructor(props) {
     super(props);
@@ -40,10 +52,29 @@ class Letter extends React.Component {
   };
   render() {
     return (
-      <div>
-        <input type="text" onChange={this.updateletter}></input>
-        <button onClick={this.MessageSending}>보내기</button>
-      </div>
+      <Wrapper>
+        <Inner>
+          <input
+            placeholder="텍스트를 입력해주세요"
+            type="text"
+            onChange={this.updateletter}
+            style={{ padding: "10px", outline: "none" }}
+          ></input>
+          <button
+            style={{
+              width: "100%",
+              backgroundColor: "#03c7f5",
+              color: "white",
+              marginTop: "10px",
+              borderRadius: "10px",
+              border: "none",
+            }}
+            onClick={this.MessageSending}
+          >
+            보내기
+          </button>
+        </Inner>
+      </Wrapper>
     );
   }
 }
